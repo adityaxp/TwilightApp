e408ee2e03b56404d088046c97997621b83dae06
Aditya <adityabalsane10@gmail.com>
2024-02-20 13:40:36 +0530

    setup: initial project setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Canvas, LinearGradient, Rect, vec } from "@shopify/react-native-skia";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Canvas style={{ flex: 1 }}>
+      <Rect x={0} y={0} width={200} height={200}>
+        <LinearGradient
+          start={vec(0, 0)}
+          end={vec(200, 200)}
+          colors={["red", "green"]}
+        />
+      </Rect>
+    </Canvas>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
