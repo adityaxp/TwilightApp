53e752beaf0f7c73a997abe97d51bba0323367a7
Aditya <adityabalsane10@gmail.com>
2024-02-22 20:36:51 +0530

    fix(app): fixed navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,40 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { HomeBackground } from "./src/components/HomeBackground";
 import WeatherTabBar from "./src/components/tabbar/WeatherTabBar";
+import WeatherInfo from "./src/components/section/WeatherInfo";
+import { currentWeather } from "./src/data/CurrentWeather";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+import { useCallback } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import ForecastSheet from "./src/components/bottomsheet/ForecastSheet";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "SF-Thin": require("./assets/fonts/SF-Pro-Display-Thin.otf"),
+    "SF-Regular": require("./assets/fonts/SF-Pro-Display-Regular.otf"),
+    "SF-Semibold": require("./assets/fonts/SF-Pro-Display-Semibold.otf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+  if (!fontsLoaded) return null;
+
   return (
-    <>
-      <HomeBackground />
-      <WeatherTabBar />
-      <StatusBar style="light" />
-    </>
+    <SafeAreaProvider onLayout={onLayoutRootView}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <HomeBackground />
+        <WeatherInfo weather={currentWeather} />
+        <ForecastSheet />
+        <WeatherTabBar />
+        <StatusBar style="light" />
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   );
 }
